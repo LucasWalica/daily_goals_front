e2edb2ce73095db324b2a553cf942f763858e4c0
LucasWalica <lucasw58@educastur.es>
2025-06-07 15:31:45 +0200

    changed to tailwind 3.4.1

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+     "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    colors: {
+        'custom-mint': '#A8E6CF',
+        'custom-sky': '#B3E5FC',
+        'custom-lavanda': '#D1C4E9',
+        'custom-melocoton': '#FFCCBC',
+        'custom-soft-grey': '#ECEFF1',
+        'custom-golden-yellow': '#FFF59D',
+      },
+    extend: {},
+  },
+  plugins: [],
+}
+
